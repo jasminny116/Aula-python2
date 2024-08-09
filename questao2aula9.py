a7739fd93948f9a965c59ea15be1a0674fc1a4a1
minny <jasminny61@gmail.com>
2024-08-09 14:36:02 -0300

    Update questao2aula9.py

--- questao2aula9.py
+++ questao2aula9.py
@@ -1,10 +1,11 @@
 #2. Um professor de Matemática deseja construir um programa para imprimir uma Progressão Aritmética (PA). 
 #Para isso, devem ser informados 3 argumentos: a) primeiro termo, b) quantidade de termos e c) razão.
 
-a = int(input("Digite o primeiro termo: "))
-b = int(input("Digite a quantidade de termos: "))
-c = int(input("Digite a razão do termo: "))
-s = 0
-for x in range(a, b + 1,c):
-    s+= x
-print(s)
+primeiro_termo= int(input("Digite o primeiro termo: "))
+quant_termo = int(input("Digite a quantidade de termos: "))
+raz = int(input("Digite a razão do termo: "))
+
+ultimo_termo = primeiro_termo +(quant_termo-1)*raz
+for termo in range(primeiro_termo,ultimo_termo+1,raz):
+    print(termo)
+print("FIM")
