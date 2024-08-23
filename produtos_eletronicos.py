6a6f72e3748c8101cc23432665ae313ce4a21d03
minny <jasminny61@gmail.com>
2024-08-23 11:37:14 -0300

    Create produtos_eletronicos.py

---
+++ produtos_eletronicos.py
@@ -0,0 +1,9 @@
+produtos = []
+while True:
+    nome = input("Nome do eletrônico: ")
+    produtos.append(nome)
+    resp = input("Deseja adicionar outro? (s|n): ").upper()
+    if resp == "N":
+        break
+
+print(produtoss)
